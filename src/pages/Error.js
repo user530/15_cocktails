66cf9f6101aaa8e7796efc6bc058b519133c5e16
TMV <m.timarsuev@gmail.com>
2022-12-23 12:48:30 +0300

    Cocktails - Pages added

---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h2>error page</h2>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleCocktail = () => {
+  return (
+    <div>
+      <h2>single cocktail page </h2>
+    </div>
+  );
+};
+
+export default SingleCocktail;
